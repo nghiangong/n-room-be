accca43d42fe17906fe1c62666d34ea4fa70a218
nghiangong <78186214+nghiangong@users.noreply.github.com>
2025-01-06 13:23:04 +0700

    update

--- src/main/java/com/nghiangong/entity/user/Tenant.java
+++ src/main/java/com/nghiangong/entity/user/Tenant.java
@@ -1,8 +1,10 @@
 package com.nghiangong.entity.user;
 
+import com.nghiangong.configuration.SpringContextHolder;
+import com.nghiangong.constant.Role;
+import com.nghiangong.email.BrevoEmailService;
 import com.nghiangong.entity.room.Contract;
-import com.nghiangong.entity.room.Room;
-import com.nghiangong.listener.TenantListener;
+import com.nghiangong.model.PasswordEncoderC;
 import jakarta.persistence.*;
 
 import lombok.AccessLevel;
@@ -11,16 +13,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
-
-import java.util.List;
-
 @Data
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
-@EntityListeners(TenantListener.class)
 public class Tenant extends User {
     @ManyToOne
     @JoinColumn(name = "rep_tenant_id")
@@ -39,4 +37,33 @@ public class Tenant extends User {
 
     @Transient
     private boolean changedEmail = false;
+
+    @PrePersist
+    public void prePersist() {
+        this.setPassword(PasswordEncoderC.encode("1"));
+        if (this.getRole() == Role.REP_TENANT) this.setRepTenant(this);
+    }
+
+    @PostPersist
+    public void afterInsert() {
+        this.setUsername("tenant_" + this.getId());
+        if (this.getRole() == Role.REP_TENANT) this.setRepTenant(this);
+
+        BrevoEmailService brevoEmailService = SpringContextHolder.getBean(BrevoEmailService.class);
+        brevoEmailService.sendAccount(this);
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        if (this.isChangedEmail() == true)
+            this.setPassword(PasswordEncoderC.encode("1"));
+    }
+
+    @PostUpdate
+    public void afterUpdate() {
+        if (this.isChangedEmail() == true) {
+            BrevoEmailService brevoEmailService = SpringContextHolder.getBean(BrevoEmailService.class);
+            brevoEmailService.sendAccount(this);
+        }
+    }
 }
